b6a2c908f13ff558455936d6fcdbde50f4849fc3
DanVinhNguyen <danvihnnguyen@gmail.com>
2022-01-13 13:48:41 -0800

    Update 4-in-a-line.py

--- 4-in-a-line.py
+++ 4-in-a-line.py
@@ -12,52 +12,71 @@ class Game:
 
 
 
-def printMatrix(matrix):
+
     
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"]
-    n = len(matrix)
-    
-    print("  1 2 3 4 5 6 7 8")
-    for i in range(n):
-        for j in range(n):
-            if j == 0:
-                print(f"{alphabet[i]}", end = " ")
-                if matrix[i][j] == None:
-                    print("_", end = " ")
-                elif matrix[i][j] == "X":
-                    print("X", end = " ")
-                elif matrix[i][j] == "O":
-                    print("O", end = " ")
-            else:
-                if matrix[i][j] == None:
-                    print("_", end = " ")
-                elif matrix[i][j] == "X":
-                    print("X", end = " ")
-                elif matrix[i][j] == "O":
-                    print("O", end = " ")
-        print()
-    print()
-    
-def evaluateBoardState():
+def evaluateBoardState(matrix, userKey):
     pass
 
     something = 0
 
     # check for if the opponent is going to win, and prioritize that over anything else
     
+    # parse through the entire matrix and look for the userKey (either O or X)
+    for i in len(matrix):
+        for j in len(matrix[i]):
+            if matrix[i][j] == userKey:
+                continue
+            #now we need to check for valid moves
     
     
     return something
 
 
-def validMoves(matrix, row, col):
+def validMoves(matrix, userKey):
     pass
+    
+    
+                
 
     '''
     Check up first
     '''
     # check for bounds first
     
+    
+
+'''
+This function does a couple things:
+    1. checks the bounds of the selected place
+    2. checks if there are any conflicts (anything other than the indicated string & None)
+    3. returns a list with the valid moves
+'''
+def checkBounds(matrix, row, col):
+    pass
+
+    # check up first
+    
+    
+
+def printMatrix(matrix):
+    
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    n = len(matrix)
+    
+    print("  1 2 3 4 5 6 7 8")
+    for row in range(n):
+        for col in range(n):
+            if col == 0:
+                print(f"{alphabet[row]}", end = " ")
+
+            if matrix[row][col] == None:
+                print("_", end = " ")
+            elif matrix[row][col] == "X":
+                print("X", end = " ")
+            elif matrix[row][col] == "O":
+                print("O", end = " ")
+        print()
+    print()
 
 
 if __name__ == "__main__":
@@ -80,4 +99,9 @@ if __name__ == "__main__":
             [None, None, None, None, None, None, None, None],
             [None, None, None, None, None, None, None, None]]
     
-    printMatrix(test)
+    row = 0
+    column = 4
+    
+    blank[row][column] = "X"
+    
+    printMatrix(blank)
